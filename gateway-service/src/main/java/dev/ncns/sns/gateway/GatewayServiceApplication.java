7f239d84e75aad7544fba85dd3102661b8b78fcc
YJeongKim <yjeongisme@gmail.com>
2021-12-30 02:12:30 +0900

    init: initialize gateway service

---
+++ gateway-service/src/main/java/dev/ncns/sns/gateway/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package dev.ncns.sns.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
