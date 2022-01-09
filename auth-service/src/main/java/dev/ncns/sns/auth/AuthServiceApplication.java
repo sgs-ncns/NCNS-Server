0ec1d6ec3cd5531916b8d5ea9d16618d80eaac96
YJeongKim <yjeongisme@gmail.com>
2022-01-09 21:04:18 +0900

    init: initialize auth service

---
+++ auth-service/src/main/java/dev/ncns/sns/auth/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package dev.ncns.sns.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
