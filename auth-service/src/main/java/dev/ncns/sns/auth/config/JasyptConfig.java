8b2cf95381e416acf9689fdc4b659ddc5f7d4277
YJeongKim <yjeongisme@gmail.com>
2022-01-13 16:29:43 +0900

    feat: jwt secret 값 암호화 및 테스트 구현

---
+++ auth-service/src/main/java/dev/ncns/sns/auth/config/JasyptConfig.java
@@ -0,0 +1,10 @@
+package dev.ncns.sns.auth.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEncryptableProperties
+@Configuration
+public class JasyptConfig {
+
+}
