60b8a3d60d9c20cd837cc0ea5f88b81a9ecc48f9
rachel5004 <j26597719@naver.com>
2022-01-24 15:27:34 +0900

    feat: 팔로우/언팔로우 api

--- user-service/src/main/java/dev/ncns/sns/user/service/FollowService.java
+++ user-service/src/main/java/dev/ncns/sns/user/service/FollowService.java
@@ -1,5 +1,6 @@
 package dev.ncns.sns.user.service;
 
+import dev.ncns.sns.user.domain.Follow;
 import dev.ncns.sns.user.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,24 @@ public class FollowService {
     @Transactional(readOnly = true)
     public List<Long> getFollowerIdList(Long userId) {
         return followRepository.findUserIdByTargetId(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public Long isFollowing(Long userId, Long targetId) {
+        return followRepository.isExist(userId, targetId).orElse(null);
+    }
+
+    @Transactional
+    public void unFollow(Long followId) {
+        followRepository.deleteById(followId);
+    }
+
+    @Transactional
+    public void requestFollow(Long userId, Long targetId) {
+        if (userId == targetId) {
+            throw new IllegalArgumentException("unvalid request");
+        }
+        Follow follow = Follow.builder().userId(userId).targetId(targetId).build();
+        followRepository.save(follow);
     }
 }
