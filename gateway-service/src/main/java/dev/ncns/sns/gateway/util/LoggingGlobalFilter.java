2cc5d10da87813bcf2ff66c0d4bb2237a8f3200a
YJeongKim <yjeongisme@gmail.com>
2022-02-04 13:38:12 +0900

    feat: 로깅 필터 구현

---
+++ gateway-service/src/main/java/dev/ncns/sns/gateway/util/LoggingGlobalFilter.java
@@ -0,0 +1,33 @@
+package dev.ncns.sns.gateway.util;
+
+import dev.ncns.sns.common.util.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingGlobalFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String curlX = String.format("-X %s %s", request.getMethod(), request.getURI());
+        String curlH = String.format("-H %s %s", Constants.AUTH_HEADER_KEY, request.getHeaders().get(Constants.AUTH_HEADER_KEY));
+
+        log.info("[Logging] - " + curlX + " " + curlH);
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+}
