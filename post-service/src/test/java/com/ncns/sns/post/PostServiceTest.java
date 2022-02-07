496d2fc446d7af46b8cebd5e8eaa1608f46a245e
rachel5004 <j26597719@naver.com>
2022-02-07 16:12:57 +0900

    test: like api 테스트 코드 추가

--- post-service/src/test/java/com/ncns/sns/post/PostServiceTest.java
+++ post-service/src/test/java/com/ncns/sns/post/PostServiceTest.java
@@ -6,7 +6,7 @@ import com.ncns.sns.post.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class PostServiceTest {
 
@@ -15,19 +15,27 @@ public class PostServiceTest {
 
     @Test
     void createPost() {
-        CreatePostRequestDto dto = new CreatePostRequestDto("2022-02-03");
+        CreatePostRequestDto dto = new CreatePostRequestDto("2022-02-07");
         postService.createPost(dto);
     }
 
     @Test
     void updatePost() {
-        UpdatePostRequestDto dto = new UpdatePostRequestDto(2L, "updated");
+        UpdatePostRequestDto dto = new UpdatePostRequestDto(4L, "updated");
         postService.updatePost(dto);
     }
 
     @Test
     void deletePost() {
-        postService.deletePost(1L);
+        postService.deletePost(5L);
     }
 
+    @Test
+    void likePost() {
+        String result = postService.requestLikePost(5L);
+        assertEquals(result, "like");
+        result = postService.requestLikePost(5L);
+        assertEquals(result, "disLike");
+
+    }
 }
