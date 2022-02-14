86e4e2e075940541b4475de61d59abfe825ecc43
rachel5004 <j26597719@naver.com>
2022-02-14 17:25:24 +0900

    feat: common ApiController

--- feed-service/src/main/java/dev/ncns/sns/feed/controller/FeedController.java
+++ feed-service/src/main/java/dev/ncns/sns/feed/controller/FeedController.java
@@ -1,28 +1,25 @@
 package dev.ncns.sns.feed.controller;
 
+import dev.ncns.sns.common.annotation.NonAuthorize;
+import dev.ncns.sns.common.controller.ApiController;
 import dev.ncns.sns.common.domain.ResponseEntity;
 import dev.ncns.sns.feed.domain.Feed;
 import dev.ncns.sns.feed.dto.request.CreateFeedDocumentRequestDto;
 import dev.ncns.sns.feed.dto.request.FollowUpdateRequestDto;
 import dev.ncns.sns.feed.dto.response.PostResponseDto;
 import dev.ncns.sns.feed.service.FeedService;
+import dev.ncns.sns.feed.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
-import dev.ncns.sns.common.annotation.NonAuthorize;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@ComponentScan(basePackages = "dev.ncns.sns.common.exception")
 @RequestMapping(value = "/api/feed")
 @RestController
-public class FeedController {
+public class FeedController extends ApiController {
 
-    @Value("${server.port}")
-    private String port;
 
     private final FeedService feedService;
 
@@ -39,8 +36,15 @@ public class FeedController {
         } catch (Exception e) {
             // TODO: 업데이트 실패 리스폰스 처리
         }
-        List<Feed> feeds = feedService.getFeeds(userId);
-        return ResponseEntity.successResponse(port, feeds);
+        List<Feed> feeds = feedService.getFeeds(SecurityUtil.getCurrentUserId());
+        return getSuccessResponse(feeds);
+    }
+
+    @NonAuthorize
+    @PostMapping
+    public ResponseEntity<Void> createFeedDocument(@Validated @RequestBody CreateFeedDocumentRequestDto dto) {
+        feedService.createFeedDocument(dto);
+        return getSuccessResponse();
     }
 
     @NonAuthorize
