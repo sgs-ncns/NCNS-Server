932afe30bd38d46e70d61cc1e504f13d912b5382
YJeongKim <yjeongisme@gmail.com>
2022-02-16 02:25:31 +0900

    chore: ElasticSearch 환경 구성

---
+++ search-service/src/main/java/dev/ncns/sns/search/config/ElasticsearchConfig.java
@@ -0,0 +1,27 @@
+package dev.ncns.sns.search.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+}
