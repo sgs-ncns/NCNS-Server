13d5ac25edb0b0fba340821f35e3b7fe6e3050ff
YJeongKim <yjeongisme@gmail.com>
2022-02-16 18:10:01 +0900

    feat: Search 도메인 정의 및 repository, service, controller 클래스 생성

--- search-service/src/main/java/dev/ncns/sns/search/config/ElasticsearchConfig.java
+++ search-service/src/main/java/dev/ncns/sns/search/config/ElasticsearchConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@EnableElasticsearchRepositories
 @Configuration
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
